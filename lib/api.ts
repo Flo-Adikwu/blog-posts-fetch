64965e78194efeb1c2aa594090bcbe726d86bf12
Florence Adikwu <florenceadikwu@gmail.com>
2025-06-20 14:34:28 +0100

    set up API layer and displayed post

--- lib/api.ts
+++ lib/api.ts
@@ -1,1 +1,36 @@
 // import axios from "axios";
+import axios from "axios";
+
+export type Post = {
+    id: number;
+    title: string;
+    body: string;
+}
+
+//Base URL
+const BASE_URL = "https://dummyjson.com";
+
+//fetch all posts
+export async function getAllPosts(): Promise<Post[]>{
+    const {data} = await axios.get(`${BASE_URL}/posts`);
+    return data.posts;
+}
+// Natale Catella
+// 1:04 PM
+// Basics
+// Use the endpoints in https://dummyjson.com/docs/posts and/or https://dummyjson.com/docs/comments for your API requests.
+// Display a list of 20 posts from the provided API
+// Each item should present: title, body, the list of tags, reactions (like, dislike), views count and comments count
+
+// Comments Basics
+// Fetch and display the comments count for each post
+// Natale Catella
+// 1:57 PM
+// Global Loading Indicator
+// Implement a loading indicator that is display while any data is being fetched
+// The loading indicator should remain visible until all posts and all comments have been fully loaded.
+// Ensure that the posts and comments are not displayed or in their loading state until the loading process is complete
+// ---
+// Styling
+// Apply styling to the work done so far: you can use any framework of your choice (Tailwind, Bootstrap, or simply CSS or SCSS)
+// Make the application visually appealing, with attention to layout, typography and spacing
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,34 @@
-export default function Home() {
+import { getAllPosts, Post } from "@/lib/api";
+import React from "react";
+
+type Props = {
+  posts: Post[];
+};
+
+export default function Posts({ posts }: Props) {
   return (
-    <div className="h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">Hello</h1>
+    <div className="max-w-3xl mx-auto p-6">
+      <h1 className="text-3xl font-bold mb-6">POSTS</h1>
+      <ul className="space-y-4">
+        {posts.map((post) => (
+          <li
+            key={post.id}
+            className="border p-4 rounded hover:shadow bg-sky-500/50"
+          >
+            <h5>{post.title}</h5>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
+}
+//prerender all posts at build time
+export async function getStaticProps() {
+  const posts = await getAllPosts();
+  return {
+    props: {
+      posts,
+    },
+  };
 }
